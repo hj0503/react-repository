0a12caf0f845d83ff62909b39f9af79d64b84651
hj0503 <857131753@qq.com>
2018-08-26 14:25:11 +0800

    react-addons-perf

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Perf } from 'react-addons-perf';
+
+//性能检测
+if (_DEV_) {      //_DEV_表示在开发环境下 
+  window.Perf = Perf
+}
+
+
 
 render(
     <div>Hello React!!!</div>,
